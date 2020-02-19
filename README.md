f0132e3b5a7cd6537da552aa63cd7b00fc1368ae
kevin-Arpe <arpeggiogio@gmail.com>
2020-02-19 16:55:14 +0900

    Create Customers.js

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Management system
 Ths project is a first react app for practice.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '홍길동',
+  'birthday': '960122',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '이순신',
+  'birthday': '931123',
+  'gender': '남자',
+  'job': '고등학생'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '킴세종',
+  'birthday': '940501',
+  'gender': '남자',
+  'job': '회사원'
+}]
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+              key={c.id}
+              id={c.id}
+              img={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
