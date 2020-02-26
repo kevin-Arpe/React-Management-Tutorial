ddea2394ddec76cbd85ce2c873f193a07bbb37e5
kevinLee <arpeggiogio@gmail.com>
2020-02-26 22:58:17 +0900

    fix yarn error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extneded: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
